3fe99afa14421bd9f471491ea9e95be1b9188881
feegow <viniciusmaia@feegow.com.br>
2020-02-21 13:46:47 -0300

    # Onde? CKEditor
    # O que?
    fix: ajusta versao do CK Editor

--- ckeditornew2/config.js
+++ ckeditornew2/config.js
@@ -1,7 +1,7 @@
 CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
 		{ name: 'undo', groups: [ 'undo' ] },
-		{ name: 'styles', groups: [ 'styles', 'align' ] },
+		{ name: 'styles', groups: [ 'styles', 'align', 'format' ] },
 		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
 		{ name: 'paragraph', groups: ['list'] },
 		{ name: 'insert', groups: [ 'insert' ] },

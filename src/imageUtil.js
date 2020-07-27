b9d47035b5c8ec7ae0498ac3c392262c13f79919
Wanderson Macedo <f_disk@hotmail.com>
2020-07-27 13:55:03 -0300

    adicionando

--- src/imageUtil.js
+++ src/imageUtil.js
@@ -4,11 +4,30 @@ $(document).ready(function(){
 });
 function getProfilePic(userId,db,table,isFunc = false)
 {
+
     let objct = new FormData();
     objct.append('userId',userId);
     objct.append('licenca' ,db);
     objct.append('folder_name' ,"Perfil");
     objct.append('userType' ,table);
+    let image = '';
+    //
+    if(localStorage.getItem('perfilImg'))
+    {
+        image = localStorage.getItem('perfilImg');
+        if(table == 'profissionais')
+        {
+            $('li a #avatarFoto').attr('src',image);
+            if(isFunc == true){
+
+                $('#divDisplayFoto #avatarFoto').attr('src',image);
+            }
+
+        }else{
+            $('#divDisplayFoto #avatarFoto').attr('src',image);
+        }
+        return false;
+    }
     $.ajax(
         {
             url: domain + "api/image/perfil",
@@ -18,17 +37,19 @@ function getProfilePic(userId,db,table,isFunc = false)
             data: objct,
             // Now you should be able to do this:
             success: function (data) {
+                image = "https://functions.feegow.com/load-image?licenseId=" + db + "&folder=Perfil&file="+ data +"&type=user"
+                localStorage.setItem("perfilImg",image);
                 if(table == 'profissionais')
                 {
-                    $('li a #avatarFoto').attr('src',data);
+                    $('li a #avatarFoto').attr('src',image);
                     if(isFunc == true){
-                        $('#divDisplayFoto #avatarFoto').attr('src',data);
+
+                        $('#divDisplayFoto #avatarFoto').attr('src',image);
                     }
 
                 }else{
-                    $('#divDisplayFoto #avatarFoto').attr('src',data);
+                    $('#divDisplayFoto #avatarFoto').attr('src',image);
                 }
-
             }
         });
 }
